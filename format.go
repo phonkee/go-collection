d8b1c168ac914e0a0f2927ef6f89f87c54516e89
Peter Vrba <peter_vrba@comcast.com>
2022-08-28 01:36:23 +0200

    minor change

--- format.go
+++ format.go
@@ -13,9 +13,9 @@ const (
 
 // Section formats section to stdout, inner function should only use fmt to print anything to stdout
 func Section(name string, in func()) {
-	header := strings.Repeat(DividerChar, PrependedDividerCount) + " " + name + " " + strings.Repeat(DividerChar, SectionHeaderWidth-len(name)+1)
-	footer := strings.Repeat(DividerChar, PrependedDividerCount) + "/" + name + " " + strings.Repeat(DividerChar, SectionHeaderWidth-len(name)+1)
-	fmt.Println(header[:80])
+	header := strings.Repeat(DividerChar, PrependedDividerCount) + " " + name + " " + strings.Repeat(DividerChar, SectionHeaderWidth)
+	footer := strings.Repeat(DividerChar, PrependedDividerCount) + "/" + name + " " + strings.Repeat(DividerChar, SectionHeaderWidth)
+	fmt.Println(header[:SectionHeaderWidth])
 	in()
-	fmt.Println(footer[:80])
+	fmt.Println(footer[:SectionHeaderWidth])
 }
