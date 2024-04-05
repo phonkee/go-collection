e6a516451c1747822bf9aa3960f936050a112520
Peter Vrba <peter_vrba@comcast.com>
2024-04-06 01:04:58 +0200

    group method implemented

---
+++ group.go
@@ -0,0 +1,15 @@
+package collection
+
+// Group elements of the collection by the result of the function
+func Group[T any, K comparable](collection Collection[T], fn func(T) K) map[K]Collection[T] {
+	// maintain the order and return
+	grouped := make(map[K]Collection[T])
+	for _, item := range collection {
+		key := fn(item)
+		if _, ok := grouped[key]; !ok {
+			grouped[key] = make(Collection[T], 0)
+		}
+		grouped[key] = append(grouped[key], item)
+	}
+	return grouped
+}
---
+++ group_test.go
@@ -0,0 +1,31 @@
+package collection
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGroup(t *testing.T) {
+
+	type TestStruct struct {
+		value int
+	}
+
+	c := New[TestStruct](
+		TestStruct{value: 1},
+		TestStruct{value: 1},
+		TestStruct{value: 2},
+		TestStruct{value: 2},
+		TestStruct{value: 3},
+		TestStruct{value: 3},
+	)
+
+	g := Group(c, func(item TestStruct) int {
+		return item.value
+	})
+
+	assert.Equal(t, 3, len(g))
+	assert.Equal(t, 2, len(g[1]))
+	assert.Equal(t, 2, len(g[2]))
+	assert.Equal(t, 2, len(g[3]))
+}
